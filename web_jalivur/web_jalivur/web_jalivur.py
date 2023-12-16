444cc98e9d8894a5e88d1fd6b66467dc8dc51b0b
Jalivur <esteya92@gmail.com>
2023-12-16 21:24:15 +0100

    plantilla creada

---
+++ web_jalivur/web_jalivur/web_jalivur.py
@@ -0,0 +1,32 @@
+"""Welcome to Reflex! This file outlines the steps to create a basic app."""
+from rxconfig import config
+
+import reflex as rx
+
+docs_url = "https://reflex.dev/docs/getting-started/introduction"
+filename = f"{config.app_name}/{config.app_name}.py"
+
+
+class State(rx.State):
+    """The app state."""
+
+    pass
+
+
+def index() -> rx.Component:
+    return rx.fragment(
+        rx.color_mode_button(rx.color_mode_icon(), float="right"),
+        rx.vstack(
+            rx.heading("Hola Jalivur!", font_size="5em"),
+        width="100%",
+            spacing="1.5em",
+            font_size="2em",
+            padding_top="10%",
+        ),
+    )
+
+
+# Add state and page to the app.
+app = rx.App()
+app.add_page(index)
+app.compile()
