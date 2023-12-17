7b7deaae570b9a893e53bdade74365ea96951800
Jalivur <esteya92@gmail.com>
2023-12-17 02:16:59 +0100

    empezamos a dar eslitos a la web

--- web_jalivur/web_jalivur/views/links/links.py
+++ web_jalivur/web_jalivur/views/links/links.py
@@ -4,8 +4,8 @@ from web_jalivur.components.link_button import link_button
 def links() -> rx.Component:
     return rx.vstack(
         link_button("Twich","https://www.twitch.tv/aestella92"),
-        link_button("GitHub", "https://github.com/Jalivur"),
-        link_button("Youtube", "https://www.youtube.com/channel/UC0doqISuLkdkmiThl7EEwBQ"),
+        link_button("GitHub", "https://github.com/Jalivur/web_python_propia"),
+        link_button("Youtube (Canal principal)", "https://www.youtube.com/channel/UC0doqISuLkdkmiThl7EEwBQ"),
         link_button("Instagram", "https://www.instagram.com/jalivur/")
 
     )
--- web_jalivur/web_jalivur/web_jalivur.py
+++ web_jalivur/web_jalivur/web_jalivur.py
@@ -6,6 +6,7 @@ from web_jalivur.components.nav_bar import navbar
 from web_jalivur.views.header.header import header
 from web_jalivur.views.links.links import links
 from web_jalivur.components.footer import footer
+import web_jalivur.styles.styles as styles
 docs_url = "https://reflex.dev/docs/getting-started/introduction"
 filename = f"{config.app_name}/{config.app_name}.py"
 
@@ -17,14 +18,23 @@ class State(rx.State):
 
 
 def index() -> rx.Component:
-    return rx.vstack(
+    return rx.box(
         navbar(),
-        header(),
-        links(),
+        rx.center(
+            rx.vstack(
+            header(),
+            links(),
+            max_width=styles.MAX_WIDTH,
+            width="100%",
+            margin_y=styles.Spacer.BIG.value
+            )
+        ),
         footer()
     )
 
 # Add state and page to the app.
-app = rx.App()
+app = rx.App(
+    style= styles.BASE_STYLE
+) 
 app.add_page(index)
 app.compile()
